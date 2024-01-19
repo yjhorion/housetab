78c339cf72a699cc753469a1c4a3b050267c714e
Jhyun Y <yjhorion@gmail.com>
2024-01-20 05:27:59 +0900

    데이터베이스 삽입 라우터 추가

--- app.js
+++ app.js
@@ -1,33 +1,19 @@
 import express from 'express'
-import xlsx from 'node-xlsx'
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
-import mysql from 'mysql2/promise'
-import dotenv from 'dotenv'
-import { prisma } from './utils/prisma/index.js'
 import {default as statistics}  from './routes/statistics.router.js'
 import {default as getOrders} from './routes/getOrder.router.js'
+import {default as insertData} from './routes/data.router.js'
 
 
 
 const app = express()
 const PORT = 3000
 
-dotenv.config()
-const DATABASE_URL = process.env.DB_URL
-const DATABASE_PW = process.env.DB_PW
 
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const example = xlsx.parse(`${__dirname}/testdata.xlsx`)  // router로 만들경우, example 변수에 parsing 되어 할당되는 xlsx 파일을 multer 로 S3에 저장했다가 불러오는 방식으로. req에서 파일을 formdata 형식으로 받을것.
-const UserData = example[0]['data']
-const OrderData = example[1]['data']
 
 app.use('/', [
     statistics,
-    getOrders
+    getOrders,
+    insertData
 ])
 
 app.listen(PORT, () => {
@@ -36,7 +22,18 @@ app.listen(PORT, () => {
 
 
 
-/* 엑셀 데이터를 db에 삽입하는 API*/
+// /* 엑셀 데이터를 db에 삽입하는 API*/
+// dotenv.config()
+// const DATABASE_URL = process.env.DB_URL
+// const DATABASE_PW = process.env.DB_PW
+
+
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = dirname(__filename)
+
+// const example = xlsx.parse(`${__dirname}/testdata.xlsx`)  // router로 만들경우, example 변수에 parsing 되어 할당되는 xlsx 파일을 multer 로 S3에 저장했다가 불러오는 방식으로. req에서 파일을 formdata 형식으로 받을것.
+// const UserData = example[0]['data']
+// const OrderData = example[1]['data']
 
 // async function createDatabase() {
 //     try {
@@ -77,27 +74,3 @@ app.listen(PORT, () => {
   
 
 //   createDatabase();
-  
-
-
-/* 월별 매출 통계 API */
-//   async function statistics() {
-//     try {
-//       const connection = await mysql.createConnection({
-//         host: DATABASE_URL,
-//         user: 'root',
-//         password: DATABASE_PW,
-//         database: 'housetab'
-//       });
-
-      
-
-
-//       await connection.end();
-//       console.log('Connection closed.');
-//     } catch (error) {
-//       console.error('Error creating database:', error);
-//     }
-//   }
-
-//   statistics()
--- routes/data.router.js
+++ routes/data.router.js
@@ -1,40 +1,78 @@
-import express from 'express'
-import xlsx from 'node-xlsx'
-import { fileURLToPath } from 'url'
+// database.router.js
+
+import express from 'express';
+import xlsx from 'node-xlsx';
+import mysql from 'mysql2/promise';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import dotenv from 'dotenv';
+import path from 'path'
+
+const router = express.Router();
+
+dotenv.config();
+
+const DATABASE_URL = process.env.DB_URL;
+const DATABASE_PW = process.env.DB_PW;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const parentDir = path.join(__dirname, '..')
+
+const example = xlsx.parse(path.join(parentDir, `testdata.xlsx`));
+const UserData = example[0]['data'];
+const OrderData = example[1]['data'];
 
 
-const router = express.Router()
+async function createDatabase() {
+  try {
+    const connection = await mysql.createConnection({
+      host: DATABASE_URL,
+      user: 'root',
+      password: DATABASE_PW,
+      database: 'housetab',
+    });
 
-router.post("/InsertData", async (req, res, next) => {
-
-    const { filePath } = req.body
-
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = dirname(__filename)
-    
-    const example = xlsx.parse(`filepath`)
-    const UserData = example[0]['data']
-    const OrderData = example[1]['data']
-    
     for (let i = 1; i < UserData.length; i++) {
-        if (!UserData[i][0]) {
-            break;
-          }    
-          const [UserId, UserName, UserGrade] = UserData[i]
-          
-          await connection.execute('INSERT INTO customer (UserId, UserName, UserGrade) VALUES (?, ?, ?)', [UserId, UserName, UserGrade])
-      }  
-      
-      for (let i = 1; i< OrderData.length; i++) {
-        if (!OrderData[i][0]) {
-            break;
-        }
-        const [UserId, OrderDate, OrderType, OrderPrice] = OrderData[i]
-        const formattedOrderDate = new Date((OrderDate - 2) * 24 * 60 * 60 * 1000 + Date.UTC(1900, 0, 1)).toISOString().slice(0,10)
-        await connection.execute('INSERT INTO orders (UserId, OrderDate, OrderType, OrderPrice) VALUES (?, ?, ?, ?)', [UserId, formattedOrderDate, OrderType, OrderPrice])
+      if (!UserData[i][0]) {
+        break;
       }
+      const [UserId, UserName, UserGrade] = UserData[i];
 
-      return res.status(201).json({ message : "Successfully Inserted into Database"})
-})
+      await connection.execute(
+        'INSERT INTO customer (UserId, UserName, UserGrade) VALUES (?, ?, ?)',
+        [UserId, UserName, UserGrade]
+      );
+    }
 
-export default router
+    for (let i = 1; i < OrderData.length; i++) {
+      if (!OrderData[i][0]) {
+        break;
+      }
+      const [UserId, OrderDate, OrderType, OrderPrice] = OrderData[i];
+      const formattedOrderDate = new Date(
+        (OrderDate - 2) * 24 * 60 * 60 * 1000 + Date.UTC(1900, 0, 1)
+      )
+        .toISOString()
+        .slice(0, 10);
+      await connection.execute(
+        'INSERT INTO orders (UserId, OrderDate, OrderType, OrderPrice) VALUES (?, ?, ?, ?)',
+        [UserId, formattedOrderDate, OrderType, OrderPrice]
+      );
+    }
+
+    await connection.end();
+    console.log('Connection closed.');
+  } catch (error) {
+    console.error('Error creating database:', error);
+  }
+}
+
+
+router.post('/create-database', async (req, res) => {
+  createDatabase(); 
+  res.status(200).json({ message: '데이터베이스에 입력이 시작되었습니다' });
+});
+
+export default router;
